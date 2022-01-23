f1a85efc6f5fb91010c8f8660e42b7d3d192231a
MariCarmenO <mariacarmen.otero@ehu.eus>
2022-01-24 00:55:43 +0100

    Second activity 1

---
+++ android-second-activity-3/app/src/main/java/ar/com/tutorialesya/proyecto012/MainActivity.java
@@ -0,0 +1,52 @@
+package ar.com.tutorialesya.proyecto012;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et1=(EditText)findViewById(R.id.editText);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void ver (View v) {
+        Intent i=new Intent(this,Actividad2.class);
+        i.putExtra("direccion", et1.getText().toString());
+        startActivity(i);
+    }
+}
